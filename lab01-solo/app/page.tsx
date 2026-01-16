0f539354f0a54f709d2d45c8ad234b59d691947c
Brian-W-Li <brian.li.fml@gmail.com>
2026-01-16 15:22:49 -0800

    Add Next.js hello world

---
+++ lab01-solo/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        fontSize: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Hello World</h1>
+    </div>
+  );
+}
